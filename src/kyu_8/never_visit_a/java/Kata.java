f3139ec37da7559b8dbfab8b79eb56a0c574408e
debugger4O4 <now-kirill@ya.ru>
2024-12-25 18:10:43 +0300

    Возвращает название фрукта.

---
+++ src/kyu_8/never_visit_a/java/Kata.java
@@ -0,0 +1,8 @@
+package kyu_8.never_visit_a.java;
+
+public class Kata {
+
+    public static String subtractSum (int n) {
+        return "apple";
+    }
+}
---
+++ src/kyu_8/never_visit_a/java/SolutionTest.java
@@ -0,0 +1,14 @@
+package kyu_8.never_visit_a.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SolutionTest {
+    @Test
+    public void basicTest() {
+//        assertEquals("apple", Kata.subtractSum(10));
+//        assertEquals("apple", Kata.subtractSum(9878));
+        assertEquals("apple", Kata.subtractSum(4401));
+    }
+}
