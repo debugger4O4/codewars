cafb4a610d1cab34c0cc2c78dbd77f90887f19ed
debugger4O4 <now-kirill@ya.ru>
2025-01-06 10:49:38 +0300

    Дебаг.

---
+++ src/kyu_8/a_plus_b/java/FirstClass.java
@@ -0,0 +1,7 @@
+package kyu_8.a_plus_b.java;
+
+public class FirstClass {
+    public static byte sum (byte a, byte b) {
+        return (byte) (a + b);
+    }
+}
---
+++ src/kyu_8/a_plus_b/java/FirstClassTest.java
@@ -0,0 +1,13 @@
+package kyu_8.a_plus_b.java;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class FirstClassTest {
+    @Test
+    public void testSum() throws Exception {
+        byte a = 1;
+        byte b = 2;
+        assertEquals(3, FirstClass.sum(a, b));
+    }
+}
