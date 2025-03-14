7d93aac0bc5f97702ecd6acde557e9468ff7c29e
seera <now-kirill@ya.ru>
2025-03-14 16:01:13 +0300

    Highest number with two prime factors.

--- src/kyu_6/highest_number_with_two_prime_factors/java/Highest2FactorsTest.java
+++ src/kyu_6/highest_number_with_two_prime_factors/java/Highest2FactorsTest.java
@@ -14,8 +14,8 @@ public class Highest2FactorsTest {
         System.out.println("Basic Tests");
         testing(Arrays.toString(Highest2Factors.highestBiPrimeFac(2, 3, 50)),
                 Arrays.toString(new long[] {48, 4, 1}));
-//        testing(Arrays.toString(Highest2Factors.highestBiPrimeFac(5, 11, 1000)),
-//                Arrays.toString(new long[] {605, 1, 2}));
+        testing(Arrays.toString(Highest2Factors.highestBiPrimeFac(5, 11, 1000)),
+                Arrays.toString(new long[] {605, 1, 2}));
         testing(Arrays.toString(Highest2Factors.highestBiPrimeFac(3, 13, 5000)),
                 Arrays.toString(new long[] {4563, 3, 2}));
         testing(Arrays.toString(Highest2Factors.highestBiPrimeFac(5, 7, 5000)),
