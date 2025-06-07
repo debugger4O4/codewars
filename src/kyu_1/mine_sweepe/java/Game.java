0766c36eb33ed1a4b4597e89cd65d0c8f3c491ef
seera <now-kirill@ya.ru>
2025-06-07 11:11:27 +0300

    Mine Sweeper.

---
+++ src/kyu_1/mine_sweepe/java/Game.java
@@ -0,0 +1,27 @@
+package kyu_1.mine_sweepe.java;
+
+/**
+ * Заглушка.
+ */
+public class Game {
+
+    public static int open (int row, int col) {
+        // Some logic;
+        return 0;
+    }
+
+    public static String newGame (String data) {
+        // Some logic;
+        return "";
+    }
+
+    public static String read (String data) {
+        // Some logic;
+        return "";
+    }
+
+    public static int getMinesN () {
+        // Some logic;
+        return 0;
+    }
+}
--- src/kyu_1/mine_sweepe/java/MineSweeperTests.java
+++ src/kyu_1/mine_sweepe/java/MineSweeperTests.java
@@ -2,7 +2,6 @@ package kyu_1.mine_sweepe.java;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
-import org.junit.runners.JUnit4;
 
 public class MineSweeperTests {
     /* Data structure for one test:
