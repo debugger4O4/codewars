d8db89d52842f66df6b7d87fd763aa15cedca5da
debugger <now-kirill@ya.ru>
2025-09-05 18:57:56 +0300

    Finding Remainder Without Using '%' Operator.

---
+++ src/kyu_7/finding_remainder_without_using_percent_operator/java/RemainderFinder.java
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/564f458b4d75e24fc9000041/train/java
+
+package kyu_7.finding_remainder_without_using_percent_operator.java;
+
+//class RemainderFinder {
+//
+//    public static SimpleInteger remainder(SimpleInteger dividend, SimpleInteger divisor) {
+//        return dividend.subtract((dividend.divide(divisor)).multiply(divisor));
+//    }
+//}
