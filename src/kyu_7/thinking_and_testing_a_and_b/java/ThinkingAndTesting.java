3e3ef72d73e45afdf2f7ad622dfcf969c8b2a720
debugger <now-kirill@ya.ru>
2025-09-09 18:32:03 +0300

    Thinking & Testing: A and B?

---
+++ src/kyu_7/thinking_and_testing_a_and_b/java/ThinkingAndTesting.java
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/56d904db9963e9cf5000037d/train/java
+
+package kyu_7.thinking_and_testing_a_and_b.java;
+
+public class ThinkingAndTesting {
+
+    public static int testAB(int a, int b) {
+        return a | b;
+    }
+}
