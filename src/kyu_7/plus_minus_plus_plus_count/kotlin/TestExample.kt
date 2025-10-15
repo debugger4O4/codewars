35b59d2714b1b4d8d9b18ec6d3e9604abfebeb53
debugger <now-kirill@ya.ru>
2025-10-15 17:49:25 +0300

    Plus - minus - plus - plus - ... - Count

--- src/kyu_7/plus_minus_plus_plus_count/kotlin/TestExample.kt
+++ src/kyu_7/plus_minus_plus_plus_count/kotlin/TestExample.kt
@@ -6,8 +6,8 @@ import org.junit.Test
 class TestExample {
     @Test
     fun testFixed() {
-//        assertEquals(0, catchSignChange(arrayOf(1, 3, 4, 5)));
-//        assertEquals(0, catchSignChange(arrayOf<Int>()));
+        assertEquals(0, catchSignChange(arrayOf(1, 3, 4, 5)));
+        assertEquals(0, catchSignChange(arrayOf<Int>()));
         assertEquals(2, catchSignChange(arrayOf(1, -3, -4, 0, 5)));
         assertEquals(3, catchSignChange(arrayOf(-47,84,-30,-11,-5,74,77)));
     }
